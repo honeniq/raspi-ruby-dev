22547f183d71e0ce0e9cefc6789b39600ba05cfe
honeniq <honeniq@gmail.com>
2014-12-12 10:03:55 +0900

    レシピ大体できた

---
+++ recipe/raspi-ruby-dev.rb
@@ -0,0 +1,38 @@
+# Example https://github.com/ryotarai/itamae/blob/master/spec/integration/recipes/default.rb#L15
+
+# apt update
+execute "apt-get update" do
+  command "sudo apt-get update"
+end
+
+# apt/yum Packages
+packages = []
+packages << 'git'
+packages << 'ruby-dev'
+packages << 'rubygems'
+packages << 'vim'
+packages << 'screen'
+
+packages.each do |pkg|
+  package pkg
+end
+
+# RubyGems
+gems = []
+gems << 'bundler'
+gems << 'pry'
+
+gems.each do |gem|
+  execute "gem install #{gem}" do
+    command "gem install #{gem}"
+    not_if "gem list | grep #{gem}"
+  end
+end
+
+# SSH authorized_keys
+directory '~/.ssh' do
+end
+
+remote_file '~/.ssh/authorized_keys' do
+  source './files/authorized_keys'
+end
